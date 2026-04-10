aca9e06ba0554d13df2c6c266aa8015254b524f7
Linda-31 <lindaxavierdhas3@gmail.com>
2026-04-10 13:19:56 +0530

    frontend link updated

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,10 @@ mongoose.connect(process.env.MONGODB_URI, {})
 
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
-app.use(cors());
+app.use(cors({
+    origin: ["https://mern-e-commerce-store-je53.onrender.com", "http://localhost:3000", "http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use(cookieParser()); 
